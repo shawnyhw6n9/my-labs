c33e9b4e8a7fcd08d1354ddeca7858e1f52697d2
shawnyhw6n9 <sunkistfish@hotmail.com>
2019-07-29 12:08:53 +0800

    Update redis HttpSession and lombok and profiles

--- sample/cap/cap-app/src/main/java/com/iisigroup/cap/app/config/RedisClusterConfiguration.java
+++ sample/cap/cap-app/src/main/java/com/iisigroup/cap/app/config/RedisClusterConfiguration.java
@@ -14,6 +14,7 @@ package com.iisigroup.cap.app.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
@@ -33,6 +34,11 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 @Configuration
 @EnableRedisHttpSession
 public class RedisClusterConfiguration {
+
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        return new JedisConnectionFactory();
+    }
 
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
